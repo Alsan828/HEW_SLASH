98ea601c64d9b00fb436b95f13ee88b7a4a829ea
绝区零高手 <837099769@qq.com>
2025-12-03 11:25:22 +0900

    1

--- DX21_05_Init/Map.h
+++ DX21_05_Init/Map.h
@@ -129,6 +129,7 @@ public:
     // Enemy creation for current map
     void CreateMapEnemies();
 
+    void ReloadCurrentMap();
     // Player respawn functionality
     void RespawnPlayer(int spawnId = -1);
 
